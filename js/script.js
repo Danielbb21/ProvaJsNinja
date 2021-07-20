008072d29bd195c870692d950a9b183d296a58a2
Daniel <danielbb2000@gmail.com>
2021-07-20 16:06:19 -0300

    doing functional requisite: add to cart without css style

--- js/script.js
+++ js/script.js
@@ -7,31 +7,111 @@
     var $gameNumbers = new DOM('[data-js="gameNumbers"]');
     var $complet = new DOM('[data-js="complet"]');
     var $clear = new DOM('[data-js="clear"]');
+    var $addCart = new DOM('[data-js="addCart"]');
+    var $cart = new DOM('[data-js="cart"]');
+    var $totalPrice = new DOM('[data-js="totalPrice"]');
+    var $priceElement = new DOM('[data-js="priceElement"]');
+
     var colorElement;
     var elementos = [];
     var chosingNumbers = [];
+    var cart = [];
+    var totalPrice = 0;
 
     $gameBtn.on('click', handlegameBtnClick);
     $complet.on('click', handleCompletNumbers);
     $clear.on('click', handleClearSelectedNumbers);
+    $addCart.on('click', handleAddCart);
 
-    function handleCompletNumbers() {
-        handleClearSelectedNumbers();
-        console.log(elementos.range, elementos.maxNumber);
-        chosingNumbers = fillNumbersIntoHtml(elementos.maxNumber, elementos.range);
-        console.log('elementos esoclhidos', chosingNumbers);
-
-        Array.prototype.forEach.call($gameNumbers.get()[0].children, function(element) {
-            for (var i = 0; i < chosingNumbers.length; i++) {
-                if (chosingNumbers[i] === +element.textContent) {
-                    element.style.backgroundColor = 'red';
-                    console.log(chosingNumbers[i], element.textContent);
-                }
-            }
-        });
+    function verifyDivContent() {
+        return $gameNumbers.get()[0].children.length > 0 ? true : false;
     }
 
+    function handleAddCart() {
+        console.log(elementos);
+        console.log(chosingNumbers.sort(comparaNumeros).toString());
+        cart.push({
+            numbers: chosingNumbers.sort(comparaNumeros).toString(),
+            type: elementos.type,
+            color: elementos.color,
+            price: elementos.price
+        });
+        console.log(cart);
+        addElementsIntoCartSection(cart[cart.length - 1]);
+        handleClearSelectedNumbers();
+    }
+
+    //Criar fragmentHtml
+    function addElementsIntoCartSection(cartElement) {
+        totalPrice += cartElement.price;
+        console.log('price', cartElement.price.toString());
+        var div = doc.createElement('div');
+        var $p = doc.createElement('p');
+        var $type = doc.createElement('span');
+        var textType = doc.createTextNode(cartElement.type);
+        var pText = doc.createTextNode(cartElement.numbers);
+        var $price = doc.createElement('span');
+
+        var textPrice = ' R$ ' + cartElement.price.toString();
+
+        $p.appendChild(pText);
+        $type.appendChild(textType);
+
+        $price.textContent = textPrice;
+
+        div.appendChild($p);
+        div.appendChild($type);
+        div.appendChild($price);
+
+        div.style.maxWidth = '234px';
+        div.style.maxHeight = '86px';
+        div.style.paddingLeft = '12px';
+        div.style.marginTop = '40px';
+
+        $p.style.fontSize = '15px';
+        $type.style.color = cartElement.color;
+        div.style.borderLeft = `4px solid ${cartElement.color}`;
+        $cart.get()[0].insertBefore(div, $priceElement.get()[0]);
+        // $cart.get()[0].appendChild(div);
+        // console.log('totalPrice', totalPrice.toString().replace('.', ','));
+        var formaredPrice = formatPriceToDisplay(totalPrice);
+        $totalPrice.get()[0].textContent = formaredPrice;
+    }
+
+    function formatPriceToDisplay(price) {
+        var priceFormated = price.toFixed(2);
+        return priceFormated.toString().replace('.', ',');
+    }
+
+    function handleCompletNumbers() {
+        try {
+            if (!verifyDivContent()) {
+                throw new Error('Nenhum jogo selecionado');
+            }
+
+            handleClearSelectedNumbers();
+            console.log(elementos.range, elementos.maxNumber);
+            chosingNumbers = fillNumbersIntoHtml(elementos.maxNumber, elementos.range);
+            console.log('elementos esoclhidos', chosingNumbers);
+
+            Array.prototype.forEach.call($gameNumbers.get()[0].children, function(element) {
+                for (var i = 0; i < chosingNumbers.length; i++) {
+                    if (chosingNumbers[i] === +element.textContent) {
+                        element.style.backgroundColor = 'red';
+                        console.log(chosingNumbers[i], element.textContent);
+                    }
+                }
+            });
+        } catch (err) {
+            alert(err.message);
+        }
+    }
+
+
     function handleClearSelectedNumbers() {
+
+
+
         var numbersQuantty = chosingNumbers.length;
         if (numbersQuantty > 0) {
             for (var i = 0; i < numbersQuantty; i++) {
@@ -41,6 +121,8 @@
             console.log(chosingNumbers);
             cleanColorFields();
         }
+
+
     }
 
     function cleanColorFields() {
